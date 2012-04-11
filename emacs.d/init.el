cabd6979e53fbd31b7387843ad9cbfce124f1118
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2012-04-11 11:22:10 +0200

    needed different package.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,17 @@
+
+; ELPA
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
+
+; Marmalade
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
+
+; Paredit activation
+(defun turn-on-paredit () (paredit-mode 1))
+(add-hook 'clojure-mode-hook 'turn-on-paredit)
+
